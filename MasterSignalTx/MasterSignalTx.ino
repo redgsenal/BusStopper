8fceca9e335da0515e2422926252d690d2072026
Reggie <reggie.senal@gmail.com>
2017-10-15 17:38:41 +0800

    Add files via upload
    add mast and slave buttons

---
+++ MasterSignalTx/MasterSignalTx.ino
@@ -0,0 +1,163 @@
+ #include <SPI.h>
+#include <RH_RF95.h>
+#include <elapsedMillis.h>
+
+/* for feather m0 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+#define RF95_FREQ 433.0
+// D6
+#define CALL_BUTTON 6
+// D10
+#define LED_INDICATOR 10
+// D13 - ON LED
+#define LED_POWER 13
+
+#define STATE_IDLE 0xA0
+#define STATE_BUTTON_PRESSED_SEND_REQUEST 0xA1
+#define STATE_RESPONSE_RECEIVED 0xA2
+#define STATE_TIMER_COUNT 0xA3
+
+#define BUTTON_PRESSED 0x01
+#define REPLY_RESPONSE 0x03
+#define VALID_RESPONSE 0x04
+#define ELAPSED_WAIT_TIME 0x05
+#define WAIT_TIME 20000 // 20 seconds
+
+#define TX_POWER_LEVEL 10
+
+const char RESPONSE_TOKEN[20] = "1234567890123456789";
+const char REQUEST_TOKEN[20] = "ABCDEFGH1JKLMNOPQRS";
+unsigned long currentState = STATE_IDLE;
+
+int callButtonState = 0;
+char* requestRxToken;
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+elapsedMillis waitingTimeElapsed;
+
+void setup() {
+  pinMode(LED_POWER, OUTPUT);  // declare LED as output
+  digitalWrite(LED_POWER, HIGH);
+  pinMode(LED_INDICATOR, OUTPUT);  // declare LED as output
+  pinMode(CALL_BUTTON, INPUT);    // declare pushbutton as input
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  // while (!Serial); uncomment when debugging
+  Serial.begin(9600);
+  delay(100);
+  out("Feather LoRa TX Test!");
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+  while (!rf95.init()) {
+    out("LoRa radio init failed");
+    while (1);
+  }
+  out("LoRa radio init OK!");
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    out("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); out(RF95_FREQ);
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(TX_POWER_LEVEL, false);
+}
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void out(String v){
+  Serial.println(v);
+}
+
+void loop() {
+  // loop here on button states
+  checkButton();
+  while(currentState == STATE_IDLE){
+    out("waiting button");
+    callButtonState = checkButton();
+    if (callButtonState == HIGH){
+      out("button pressed");
+      currentState = STATE_BUTTON_PRESSED_SEND_REQUEST;
+    }    
+  }
+
+  while(currentState == STATE_BUTTON_PRESSED_SEND_REQUEST){
+    out("button pressed; send signal");
+    blinkState();
+    sendRequest();
+  }
+
+  while(currentState == STATE_RESPONSE_RECEIVED){    
+    blinkState();
+    if (waitingTimeElapsed > WAIT_TIME){
+      // waiting time; reset to idle state
+      waitingTimeElapsed = 0;       // reset the counter to 0 so the counting starts over...
+      digitalWrite(LED_POWER, HIGH);
+      currentState = STATE_IDLE;
+    }   
+  }
+}
+
+void blinkState(){
+  int ledState = digitalRead(LED_POWER);  // read input value
+  ledState = (ledState == HIGH) ? LOW : HIGH;
+  digitalWrite(LED_POWER, ledState);
+  delay(500);  
+}
+
+int checkButton(){
+  int ledState = digitalRead(CALL_BUTTON);  // read input value
+  digitalWrite(LED_INDICATOR, ledState);  // turn LED OFF
+  return ledState;
+}
+
+void sendRequest() {
+  out("Sending to rf95_server");
+  // Send a message to rf95_server
+  
+  char radiopacket[] = "ABCDEFGH1JKLMNOPQRS"; //"Hello World #      ";
+  //itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); out(radiopacket);
+  radiopacket[19] = 0;
+  
+  out("Sending..."); delay(10);
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  out("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  out("Waiting for reply..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Got reply: ");
+      requestRxToken = (char*)buf;
+      out(requestRxToken);
+      Serial.print("RSSI: ");
+      out(rf95.lastRssi(), DEC);
+      if (strcmp(requestRxToken, RESPONSE_TOKEN) == 0){  
+        currentState = STATE_RESPONSE_RECEIVED;
+        waitingTimeElapsed = 0;
+        out("valid token");
+      }else{
+        out("invalid token");
+      }
+    } else {
+      out("Receive failed");
+    }
+  } else {
+    out("No reply, is there a listener around?");
+  }  
+}
+
---
+++ SlaveSignalRxLCD/SlaveSignalRxLCD.ino
@@ -0,0 +1,162 @@
+// Feather9x_RX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (receiver)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_TX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <elapsedMillis.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+/* for feather m0 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 433.0
+#define TX_POWER_LEVEL 10
+// power and state indicator
+#define LED_POWER 13
+#define WAIT_TIME 40000 // 20 seconds
+#define STATE_IDLE 0xA0
+#define STATE_REQUEST_SIGNAL_VALID 0xA1
+#define STATE_TIMER_COUNT 0xA2
+unsigned long currentState = STATE_IDLE;
+
+char* requestRxToken;
+const char RESPONSE_TOKEN[20] = "1234567890123456789";
+const char REQUEST_TOKEN[20] = "ABCDEFGH1JKLMNOPQRS";
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+LiquidCrystal_I2C lcd(0x3f,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+elapsedMillis waitingTimeElapsed;
+uint8_t c = 0;
+void clearlcd(){
+  lcd.init();
+  lcd.backlight();
+}
+void countlcd(){
+  lcd.setCursor(9,2);
+  lcd.print(++c);   
+} 
+void text(char txt[], uint8_t col = 0, uint8_t row = 0, boolean doclear = false){
+  if (doclear){
+    clearlcd();
+  }
+  lcd.setCursor(col,row);
+  lcd.print(txt);
+}
+void setup() {
+  pinMode(LED_POWER, OUTPUT);     
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  // while (!Serial); uncomment when debugging
+  Serial.begin(9600);
+  delay(100);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    out("LoRa radio init failed");
+    while (1);
+  }
+  out("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    out("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(TX_POWER_LEVEL, false);
+  
+  text("*** Welcome ***",2,0, true);
+  text(">Waiting for signal<",0,1);
+}
+
+void loop() {
+  while(currentState == STATE_IDLE){
+      // out("idle state");
+      if (rf95.available()){
+        // Should be a message for us now   
+        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+        uint8_t len = sizeof(buf);      
+        if (rf95.recv(buf, &len)) {
+          digitalWrite(LED_POWER, HIGH);
+          RH_RF95::printBuffer("Received: ", buf, len);
+          Serial.print("Got: ");
+          requestRxToken = (char*)buf;
+          Serial.println(requestRxToken);
+          // compare token if valid
+          if (strcmp(requestRxToken, REQUEST_TOKEN) == 0){
+            Serial.print("RSSI: ");
+            Serial.println(rf95.lastRssi(), DEC);
+            delay(10);
+            // Send a reply
+            delay(200); // may or may not be needed
+            // replay validation message
+            uint8_t data[] = "1234567890123456789";
+            rf95.send(data, sizeof(data));
+            rf95.waitPacketSent();
+            out("Sent a reply");
+            digitalWrite(LED_POWER, LOW);
+            out(">> VALIDATION SENT BACK");
+            currentState = STATE_REQUEST_SIGNAL_VALID;
+            waitingTimeElapsed = 0;
+            text("> Signal found <",2,0,true);
+            text(" Waiting ",5,1);
+            c = 0;
+          } else {
+            out("invalid token");
+          }
+        } else {
+          out("Receive failed");
+        }
+    }
+  }
+
+  while(currentState == STATE_REQUEST_SIGNAL_VALID){
+    // blink LED
+    // out("blink here; start time elapse count");
+    delay(1000);
+    blinkState();
+    countlcd();
+    if (waitingTimeElapsed > WAIT_TIME){
+      // waiting time; reset to idle state
+      waitingTimeElapsed = 0;       // reset the counter to 0 so the counting starts over...
+      digitalWrite(LED_POWER, HIGH);
+      currentState = STATE_IDLE;
+      c = 0;
+      text("> Doors closed <",2,0, true);
+      text(">Waiting for signal<",0,1);
+    }
+  }
+}
+
+void out(String v){
+  Serial.println(v);
+}
+
+void blinkState(){
+  int ledState = digitalRead(LED_POWER);  // read input value
+  ledState = (ledState == HIGH) ? LOW : HIGH;
+  digitalWrite(LED_POWER, ledState);
+}
